cc4408f9b5b0b07c38a6b188f89285e759fbafdb
Bryan Lo <byklo@uwaterloo.ca>
2017-11-05 22:24:45 -0500

    cors

--- src/main/java/com/partytrade/Endpoint.java
+++ src/main/java/com/partytrade/Endpoint.java
@@ -4,8 +4,17 @@ import akka.actor.ActorSystem;
 import akka.actor.ActorRef;
 import akka.http.javadsl.server.HttpApp;
 import akka.http.javadsl.server.Route;
+import akka.http.javadsl.server.RejectionHandler;
+import akka.http.javadsl.server.ExceptionHandler;
+import akka.http.javadsl.server.Directives;
+import akka.http.javadsl.model.StatusCodes;
+import akka.http.javadsl.model.headers.HttpOrigin;
+import akka.http.javadsl.model.headers.HttpOriginRange;
 import akka.pattern.Patterns;
 import static akka.http.javadsl.unmarshalling.Unmarshaller.entityToString;
+import ch.megard.akka.http.cors.javadsl.settings.CorsSettings;
+import static ch.megard.akka.http.cors.javadsl.CorsDirectives.cors;
+import static ch.megard.akka.http.cors.javadsl.CorsDirectives.corsRejectionHandler;
 
 import scala.concurrent.Future;
 import scala.concurrent.Await;
@@ -18,6 +27,9 @@ import com.partytrade.model.Order;
 import com.google.gson.Gson;
 
 import java.math.BigDecimal;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 /*
 ENDPOINT
@@ -33,7 +45,19 @@ public class Endpoint {
   class HttpServer extends HttpApp {
     @Override
     protected Route routes() {
-      return route(
+
+      final CorsSettings settings = CorsSettings.defaultSettings();
+      final RejectionHandler rejectionHandler = corsRejectionHandler().withFallback(RejectionHandler.defaultHandler());
+      final ExceptionHandler exceptionHandler = ExceptionHandler.newBuilder()
+        .match(NoSuchElementException.class, ex -> complete(StatusCodes.NOT_FOUND, ex.getMessage()))
+        .build();
+      final Function<Supplier<Route>, Route> handleErrors = inner -> Directives.allOf(
+        s -> handleExceptions(exceptionHandler, s),
+        s -> handleRejections(rejectionHandler, s),
+        inner
+      );
+
+      return cors(settings, () -> handleErrors.apply(() -> route(
         post( () ->
           path("trade", () ->
             // curl --data "{'type': 'LIMIT', 'units': '-50', 'accountId': '32158315', 'price': '4324'}" localhost:8080/trade
@@ -73,7 +97,7 @@ public class Endpoint {
             return complete(price.toString());
           })
         )
-      );
+      )));
     }
   }
 
